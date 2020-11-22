8147849ddb256c6771d2acf97afdb6c7ccd8afbc
tuankha17490 <tuankha1749@gmail.com>
2020-11-23 06:02:06 +0700

    Finish

--- pages/sql-injection/introducing-code.vue
+++ pages/sql-injection/introducing-code.vue
@@ -43,6 +43,70 @@ export default {
 </script>
 
 <style scoped>
+strong {
+  width: 300px;
+  height: fit-content;
+  display: block;
+  margin-left: 200px;
+  font-size: initial;
+  background-color: burlywood;
+  padding: 15px;
+  border-radius: 25px;
+}
+strong span {
+  font-weight: lighter;
+}
+.step-2 {
+  margin-top: 600px;
+  margin-left: 400px;
+}
+.wrap iframe,
+.fade {
+  margin-left: 200px;
+  margin-top: 220px;
+}
+.fade,
+iframe {
+  width: 700px;
+  height: 320px;
+  top: 0;
+  left: 0;
+}
+.fade {
+  position: absolute;
+  background-color: #333;
+  opacity: 0.6;
+  z-index: 10;
+}
+iframe {
+  overflow: hidden;
+  border: none;
+  position: absolute;
+}
+.wrap,
+.wrap-log {
+  position: relative;
+}
+.back,
+.next {
+  background: rgba(250, 173, 20, 0.2);
+  padding: 15px;
+  border: none;
+  cursor: pointer;
+  border-radius: 10px;
+  color: #333;
+}
+
+.next:hover,
+.back:hover {
+  color: white;
+  background-color: #333;
+  opacity: 0.7;
+}
+.wrap-button {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
 .wrap-code {
   height: 180px;
   background-color: #000000c7;
